c4bbb15c9dcf2dff9a0f32d8194db5169f411dd1
Devinc321 <devinmcrews@gmail.com>
2021-06-18 21:18:25 -0400

    I added pages package and designed one feature file

---
+++ src/test/java/step_definitions/FinomGroupTabFunctionality.java
@@ -0,0 +1,25 @@
+package step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class FinomGroupTabFunctionality {
+	
+
+	@Given("I am on the Stock Trading Tracking application homepage")
+	public void i_am_on_the_stock_trading_tracking_application_homepage() {
+	   
+	}
+	@When("I click on the tab titled {string}")
+	public void i_click_on_the_tab_titled(String string) {
+	    
+	}
+	@Then("I should be directed to {string}")
+	public void i_should_be_directed_to(String string) {
+	    
+	}
+
+
+
+}
